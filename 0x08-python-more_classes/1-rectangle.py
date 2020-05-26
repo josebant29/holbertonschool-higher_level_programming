c3ad72fa9da355212561768a7d4b018a00767f23
“josebant29” <josebant29@gmail.com>
2020-05-25 23:11:54 -0500

    x

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,44 +1,39 @@
 #!/usr/bin/python3
+
+"""module: 1-rectangle"""
+
+
 class Rectangle:
-    """This class is for manage a rectangle"""
-
+    """class: Rectangle"""
     def __init__(self, width=0, height=0):
-        """Set a objet with width and height"""
-
-        self.height = height
-        self.width = width
+        """init: self, width, height"""
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
-        """Put width"""
-
+        """property: width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """set the width in the object"""
-
+        """setter: width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
-        """Put height"""
-
+        """property: height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Set the height of the object"""
-
+        """setter:height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
-
         if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value

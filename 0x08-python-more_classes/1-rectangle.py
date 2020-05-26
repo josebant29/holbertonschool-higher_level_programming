7483933e09df133995613a4b9a0eaf675e41e2c1
“josebant29” <josebant29@gmail.com>
2020-05-25 23:12:59 -0500

    x

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,39 +1,43 @@
 #!/usr/bin/python3
-
-"""module: 1-rectangle"""
+'''Class rectangle'''
 
 
 class Rectangle:
-    """class: Rectangle"""
+    '''Form'''
+
     def __init__(self, width=0, height=0):
-        """init: self, width, height"""
-        self.__width = width
-        self.__height = height
+        '''Constructor'''
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """property: width"""
+        '''Get the width'''
         return self.__width
 
     @width.setter
     def width(self, value):
-        """setter: width"""
+        '''Set the width'''
         if type(value) is not int:
-            raise TypeError("width must be an integer")
+            raise TypeError('width must be an integer')
+
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError('width must be >= 0')
+
         self.__width = value
 
     @property
     def height(self):
-        """property: height"""
+        '''Get the height'''
         return self.__height
 
     @height.setter
     def height(self, value):
-        """setter:height"""
+        '''Set the height'''
         if type(value) is not int:
-            raise TypeError("height must be an integer")
+            raise TypeError('height must be an integer')
+
         if value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError('height must be >= 0')
+
         self.__height = value

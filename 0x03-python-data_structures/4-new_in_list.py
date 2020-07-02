e68b72e9d7f1c3ff4014cd26298be8bd61f64f67
“josebant29” <josebant29@gmail.com>
2020-07-02 15:19:07 -0500

    x

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-
-
 def new_in_list(my_list, idx, element):
-    cp_list = my_list[:]
+    new_list = my_list[:]
 
-    if idx < 0 or idx >= len(my_list):
-        return cp_list
+    for i in my_list:
+        if idx < 0:
+            return my_list
+        elif idx >= len(my_list):
+            return my_list
+        else:
+            new_list[idx] = element
+            return new_list
 
-    cp_list[idx] = element
-    return cp_list
+
+
